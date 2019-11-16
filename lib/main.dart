8e0b478f126d74730b633d4b5e98be58a95720d7
Nicolò Santamaria <nicolo.santamaria@gmail.com>
2019-11-16 15:02:11 +0100

    Fix indentation

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,15 @@ class MyHomePage extends StatefulWidget {
 	_MyHomePageState createState() => _MyHomePageState();
 }
 
+// The main basically
 class _MyHomePageState extends State<MyHomePage> {
+	Stream<List<int>> stream;
+	StreamSubscription<List<int>> listener;
+	List<int> currentSamples;
+
+	// Refreshes the Widget for every possible tick to force a rebuild of the sound wave (probably useless in my case)
+	AnimationController controller;
+
 	// TODO: rename this into pitch.
 	int _counter = 0;
 	// String note;
@@ -43,47 +51,46 @@ class _MyHomePageState extends State<MyHomePage> {
 
 	@override
 	Widget build(BuildContext context) {
-	final mainTextTheme = Theme.of(context).textTheme.apply(
-	  bodyColor: Colors.white,
-	  displayColor: Colors.white,
-	);
+		final mainTextTheme = Theme.of(context).textTheme.apply(
+		  bodyColor: Colors.white,
+		  displayColor: Colors.white,
+		);
 
-	// This method is rerun every time setState is called, for instance as done
-	// by the _incrementCounter method above.
-	return Scaffold(
-	  backgroundColor: Colors.black,
-	  body: Center(
-		child: Column(
-			mainAxisAlignment: MainAxisAlignment.center,
-			children: <Widget>[
+		// This method is rerun every time setState is called, for instance as done
+		// by the _incrementCounter method above.
+		return Scaffold(
+			backgroundColor: Colors.black,
+			body: Center(
+				child: Column(
+					mainAxisAlignment: MainAxisAlignment.center,
+					children: <Widget>[
+						Text(
+							'$_counter',
+							style: TextStyle(
+							color: Colors.white,
+							fontSize: 90,
+							fontWeight: FontWeight.bold,
+						  )
+						),
 
-			Text(
-			  '$_counter',
-			  style: TextStyle(
-				color: Colors.white,
-				fontSize: 90,
-				fontWeight: FontWeight.bold,
-			  )
+						Text(
+							'Some useful info here...',
+							style: TextStyle(
+								color: Colors.white,
+								fontSize: 18
+							)
+						),
+					],
+				),
 			),
 
-			Text(
-			  'Some useful info here...',
-				style: TextStyle(
-					color: Colors.white,
-					fontSize: 18
-				)
-			),
-		  ],
-		),
-	  ),
-
-	  floatingActionButton: FloatingActionButton.extended(
-		// TODO: change this with the proper function.
-		onPressed: _incrementCounter,
-		tooltip: 'Increment',
-		label: Text("Settings"),
-		icon: Icon(Icons.settings),
-	), // This trailing comma makes auto-formatting nicer for build methods.
-	);
+			floatingActionButton: FloatingActionButton.extended(
+				// TODO: change this with the proper function.
+				onPressed: _incrementCounter,
+				tooltip: 'Increment',
+				label: Text("Settings"),
+				icon: Icon(Icons.settings),
+			), // This trailing comma makes auto-formatting nicer for build methods.
+		);
 	}
 }

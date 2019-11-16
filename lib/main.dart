ca054dd4bf6f884cee02be8cbc25636178d39739
Nicolò Santamaria <nicolo.santamaria@gmail.com>
2019-11-16 18:32:41 +0100

    Better design of the tuner state

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
+import 'dart:ffi';
+import 'dart:async';
 import 'package:flutter/material.dart';
-import './mic.dart';
+import 'package:mic_stream/mic_stream.dart';
+
+typedef init_pitch = void Function(Int32 sampleRate, Int32 bufSize, Int32 gate);
+typedef InitPitchFunc = void Function(int sampleRate, int bufSize, bool gate);
+
+typedef get_pitch = Float Function(Int16 input, Int32 size);
+typedef GetPitchFunc = double Function(List<int> input, int size);
+
+typedef get_note_index = Int32 Function(Float pitch, Int32 referencePitch);
+typedef GetNoteIndexFunc = int Function(double pitch, int referencePitch);
+
+typedef dispose_pitch = Void Function();
+typedef DisposePitchFunc = void Function();
 
 void main() => runApp(MyApp());
 
@@ -21,31 +35,75 @@ class MyHomePage extends StatefulWidget {
 	final String title;
 
 	@override
-	_MyHomePageState createState() => _MyHomePageState();
+	_TunerState createState() => _TunerState();
 }
 
 // The main basically
-class _MyHomePageState extends State<MyHomePage> {
+class _TunerState extends State<MyHomePage> {
+	static const AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT;
+	static const SAMPLE_RATE = 48000;
+	final notes = ["...", "A", "A#", "B", "C", "D", "D#", "E", "F", "F#", "G", "G#"];
+	final dylib = DynamicLibrary.open('pitch.dylib');
+
 	Stream<List<int>> stream;
 	StreamSubscription<List<int>> listener;
 	List<int> currentSamples;
 
-	// Refreshes the Widget for every possible tick to force a rebuild of the sound wave (probably useless in my case)
-	AnimationController controller;
+	int referencePitch = 440;
+	bool isRecording = false;
+	double _freq;
+	String _note;
 
-	// TODO: rename this into pitch.
-	int _counter = 0;
-	// String note;
+	GetPitchFunc getPitch;
+	GetNoteIndexFunc getNoteIndex;
 
-	void _incrementCounter() {
-	// var mstr = new MicStream();
-	// mstr.startRecording();
+	_TunerState() {
+		// TODO: fix this native functions calling
+		final initPitchPointer = dylib.lookup<NativeFunction<init_pitch>>('init_pitch');
+		final InitPitchFunc initPitch = initPitchPointer.asFunction<InitPitchFunc>();
+
+		final getPitchPointer = dylib.lookup<NativeFunction<get_pitch>>('get_pitch');
+		getPitch = getPitchPointer.asFunction<GetPitchFunc>();
+
+		final getNoteIndexFuncPointer = dylib.lookup<NativeFunction<get_note_index>>('get_node_index');
+		getNoteIndex = getNoteIndexFuncPointer.asFunction<GetNoteIndexFunc>();
+
+//		initPitch(SAMPLE_RATE, 1024, false);
+		startRecording();
+	}
+
+	_updateData(List<int> samples) {
+		var pitch = getPitch(currentSamples, 1024);
+		var noteIdx = getNoteIndex(pitch, referencePitch);
+
+		setState(() {
+			_note = notes[noteIdx];
+			_freq = pitch;
+		});
+	}
 
 
-	setState(() {
-	  _counter++;
-	});
-  }
+	startRecording() {
+		if (!isRecording) {
+			isRecording = true;
+			stream = microphone(
+				audioSource: AudioSource.DEFAULT,
+				sampleRate: SAMPLE_RATE,
+				channelConfig: ChannelConfig.CHANNEL_IN_MONO,
+				audioFormat: AUDIO_FORMAT
+			);
+
+			listener = stream.listen(_updateData);
+		}
+	}
+
+	stopRecording() {
+		if (isRecording) {
+			isRecording = false;
+			listener.cancel();
+		}
+	}
+
 
 	@override
 	Widget build(BuildContext context) {
@@ -63,16 +121,16 @@ class _MyHomePageState extends State<MyHomePage> {
 					mainAxisAlignment: MainAxisAlignment.center,
 					children: <Widget>[
 						Text(
-							'$_counter',
+							'$_note',
 							style: TextStyle(
-							color: Colors.white,
-							fontSize: 90,
-							fontWeight: FontWeight.bold,
-						  )
+								color: Colors.white,
+								fontSize: 90,
+								fontWeight: FontWeight.bold,
+							)
 						),
 
 						Text(
-							'Some useful info here...',
+							'$_freq Hz',
 							style: TextStyle(
 								color: Colors.white,
 								fontSize: 18
@@ -84,7 +142,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
 			floatingActionButton: FloatingActionButton.extended(
 				// TODO: change this with the proper function.
-				onPressed: _incrementCounter,
+				onPressed: () => {},
 				tooltip: 'Increment',
 				label: Text("Settings"),
 				icon: Icon(Icons.settings),
